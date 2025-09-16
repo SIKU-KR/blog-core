b180bbac0306b8f262640209ed5238a4e2bf583a
Bumshik Park <peter012677@naver.com>
2025-09-16 17:23:34 +0900

    Add ApplicationContext injection and verify it's not null in tests

--- src/test/java/park/bumsiku/ServerApplicationTests.java
+++ src/test/java/park/bumsiku/ServerApplicationTests.java
@@ -1,12 +1,20 @@
 package park.bumsiku;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import park.bumsiku.config.AbstractTestSupport;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ServerApplicationTests extends AbstractTestSupport {
 
+    @Autowired
+    private ApplicationContext context;
+
     @Test
     void contextLoads() {
+        assertNotNull(context);
     }
 
 }
